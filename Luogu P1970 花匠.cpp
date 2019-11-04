7ab3f1d3a3fb1c3d2ac4cfe540350ae9dcd1dee8
PeacherMZ <55446520+PeacherMZ@users.noreply.github.com>
2019-11-04 20:17:54 +0800

    随机化算法

--- Luogu P1970 花匠.cpp
+++ Luogu P1970 花匠.cpp
@@ -6,7 +6,7 @@ int main(){
 	f[1][0]=f[1][1]=1;
 	for(int i=1;i<=n;i++){
 		scanf("%d",&a[i]);
-		for(int j=1;j<=i-1;j++){
+		for(int j=max(1,i-100);j<=i-1;j++){
 			if(a[i]>a[j])f[i][0]=max(f[i][0],f[j][1]+1);
 			if(a[i]<a[j])f[i][1]=max(f[i][1],f[j][0]+1);
 		}
